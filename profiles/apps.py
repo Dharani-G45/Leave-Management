30c53948cf8d339b39b800952af152f76aafe7f8
Dharani R <dharanir@quodeworks.com>
2026-06-09 17:38:58 +0530

    Add auto-migrate to apps.py

--- profiles/apps.py
+++ profiles/apps.py
@@ -1,5 +1,16 @@
-from django.apps import AppConfig
 
+from django.apps import AppConfig
+from django.core.management import call_command
+import os
 
 class ProfilesConfig(AppConfig):
-    name = 'profiles'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'profiles' 
+
+    def ready(self):
+        if 'DATABASE_URL' in os.environ:
+            try:
+                print("Running migrations automatically...")
+                call_command('migrate', '--noinput')
+            except Exception as e:
+                print(f"Migration error: {e}")
