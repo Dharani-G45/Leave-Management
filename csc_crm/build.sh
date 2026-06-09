00503e9fd5a867564c21f5e5fd07141c76110a9f
Dharani R <dharanir@quodeworks.com>
2026-06-09 15:54:45 +0530

    Migrate Added

--- csc_crm/build.sh
+++ csc_crm/build.sh
@@ -3,4 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --noinput
+
+python manage.py sqlmigrate profiles 0001
