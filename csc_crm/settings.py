5457bea241e5b4fd932a12561f3a2c8852e94ca5
Dharani R <dharanir@quodeworks.com>
2026-06-18 10:42:32 +0530

    Update URL

--- csc_crm/settings.py
+++ csc_crm/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a9e#4j@z2xzg1!ik=s-@xv((on564h#j5hrkaio3)8arb510ml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = [
 #     'web-production-72b49.up.railway.app', 
